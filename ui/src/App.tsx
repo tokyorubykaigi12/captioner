21f39a6b5231786e704013335f835b20be1c1040
terfno <terfno@cookpad.com>
2024-12-24 11:48:57 +0900

    :shower: remove extra

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import './App.css';
 
 function App() {
   useEffect(() => {
@@ -7,20 +6,8 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hi, Tokyo RubyKaigi 12.
     </div>
   );
 }
