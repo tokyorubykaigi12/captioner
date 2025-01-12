3690f87eada46f0a3bca3499b1762ec7097b5b0c
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-01-12 21:43:46 +0900

    Extract transcribe/translate engines to their own file

--- aws-captioner/captioner.rb
+++ aws-captioner/captioner.rb
@@ -2,84 +2,12 @@
 
 # https://github.com/ruby-no-kai/signage-app/blob/main/caption/serve.rb
 
-require 'aws-sdk-transcribestreamingservice'
-require 'aws-sdk-translate'
-
 require_relative './io'
 require_relative './watchdog'
+require_relative './transcribe_engine'
+require_relative './translate_engine'
 
 CaptionData = Data.define(:result_id, :is_partial, :transcript, :translated_transcript)
-
-class TranslateEngine
-  def initialize
-    @client = Aws::Translate::Client.new(region: 'ap-northeast-1')
-  end
-
-  def translate(text:)
-    @client.translate_text(
-      text: text,
-      source_language_code: 'ja',
-      target_language_code: 'en',
-      settings: {
-        formality: 'FORMAL'
-      }
-    ).translated_text
-  end
-end
-
-class TranscribeEngine
-  def initialize
-    @client = Aws::TranscribeStreamingService::AsyncClient.new(region: 'ap-northeast-1')
-    @input_stream = Aws::TranscribeStreamingService::EventStreams::AudioStream.new
-    @output_stream = Aws::TranscribeStreamingService::EventStreams::TranscriptResultStream.new
-
-    @output_stream.on_bad_request_exception_event do |exception|
-      raise exception
-    end
-
-    @output_stream.on_event do |event|
-      p event unless event.is_a?(Aws::TranscribeStreamingService::Types::TranscriptEvent)
-    end
-  end
-
-  attr_reader :output_stream
-
-  def feed(audio_chunk)
-    @input_stream.signal_audio_event_event(audio_chunk: audio_chunk)
-    self
-  rescue Seahorse::Client::Http2ConnectionClosedError
-    @client.connection.errors.each do |e|
-      p e
-    end
-    raise
-  end
-
-  def start
-    @client.start_stream_transcription(
-      language_code: 'ja-JP',
-      media_encoding: "pcm",
-      media_sample_rate_hertz: 16000,
-
-      enable_partial_results_stabilization: true,
-      partial_results_stability: 'high',
-
-      vocabulary_name: 'trk12', # custom vocabulary
-
-      input_event_stream_handler: @input_stream,
-      output_event_stream_handler: @output_stream,
-    )
-  end
-
-  def finish
-    @input_stream.signal_end_stream
-  end
-
-  def on_transcript_event(&block)
-    output_stream.on_transcript_event_event(&block)
-    self
-  end
-end
-
 
 watchdog = Watchdog.new(enabled: ARGV.delete('--watchdog'))
 watchdog.start()
