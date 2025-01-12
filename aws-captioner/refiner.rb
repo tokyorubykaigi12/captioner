2a9ff70fff8ba79fa69fcfc2f22fded5a2e45f32
Daisuke Aritomo <osyoyu@osyoyu.com>
2025-01-12 23:00:55 +0900

    Refiner: Impl Anthropic API backend
    Work around Bedrock's strict quota limits (1 req/minute for our AWS
    account).

--- aws-captioner/refiner.rb
+++ aws-captioner/refiner.rb
@@ -1,13 +1,52 @@
+require 'anthropic'
 require 'aws-sdk-bedrockruntime'
 
 class Refiner
-  def initialize
-    @bedrock = Aws::BedrockRuntime::Client.new(region: 'ap-northeast-1')
+  def initialize(backend: :anthropic)
+    @backend = backend
+
+    case @backend
+    in :anthropic
+      @anthropic = Anthropic::Client.new(
+        access_token: ENV.fetch('ANTHROPIC_API_KEY'),
+        anthropic_version: '2023-06-01'
+      )
+      @bedrock = nil
+    in :bedrock
+      @anthropic = nil
+      @bedrock = Aws::BedrockRuntime::Client.new(region: 'ap-northeast-1')
+    end
   end
 
-  def refine(text)
+  def refine(transcript)
+    case @backend
+    in :anthropic
+      refine_anthropic(transcript)
+    in :bedrock
+      refine_bedrock(transcript)
+    end
+  end
+
+  def refine_anthropic(transcript)
+    # Skip if the transcript is too short. It won't produce good results anyway.
+    if transcript.length < 20
+      return nil
+    end
+
+    response = @anthropic.messages(
+      parameters: {
+        model: 'claude-3-5-sonnet-20240620',
+        messages: messages(transcript),
+        max_tokens: 1000,
+      }
+    )
+
+    response["content"].first["text"]
+  end
+
+  def refine_bedrock(transcript)
     # Skip if the transcription is too short. It won't produce good results anyway.
-    if text.length < 20
+    if transcript.length < 20
       return nil
     end
 
@@ -19,22 +58,7 @@ class Refiner
         body: JSON.generate({
           anthropic_version: "bedrock-2023-05-31",
           max_tokens: 1000,
-          messages: [
-            {
-              role: "user",
-              content: <<~PROMPT
-                これはRubyに関するカンファレンスの発表の書き起こしです。以下のルールにしたがって、書き起こしを読みやすくしてください。
-
-                - 「えっと」や「まー」などのフィラーを取り除いてください
-                - Ruby に関連しそうな用語は正しい表記にしてください（「ルビー」→  Ruby など）
-                - 以上に述べた変更以外は行わず、なるべく元の書き起こしをそのまま出力する。
-
-                読みやすくなった書き起こしのみを出力すること。
-
-                #{text}
-              PROMPT
-            }
-          ]
+          messages: messages(transcript),
         })
       )
     rescue Aws::BedrockRuntime::Errors::ServiceError => e
@@ -46,5 +70,24 @@ class Refiner
     response_io = invocation.body
     response = response_io.string
     JSON.parse(response)["content"].first["text"]
+  end
+
+  def messages(transcript)
+    [
+      {
+        role: "user",
+        content: <<~__PROMPT__
+          これはRubyに関するカンファレンスの発表の書き起こしです。以下のルールにしたがって、書き起こしを読みやすくしてください。
+
+          - 「えっと」や「まー」などのフィラーを取り除いてください
+          - Ruby に関連しそうな用語は正しい表記にしてください（「ルビー」→  Ruby など）
+          - 以上に述べた変更以外は行わず、なるべく元の書き起こしをそのまま出力する。
+
+          読みやすくなった書き起こしのみを出力すること。
+
+          #{transcript}
+        __PROMPT__
+      }
+    ]
   end
 end
